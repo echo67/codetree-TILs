ab67a0226b85de9507e4e99c729ce5541f422717
echo67 <43358633+echo67@users.noreply.github.com>
2024-09-12 22:17:49 +0900

    Title: 앞에서부터 삭제하기 2, Time: 165ms, Memory: 31MB, Status: Passed - CodeTree

--- 240912/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240912/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -5,12 +5,15 @@ arr = list(map(int, input().split()))
 pq = []
 
 ans = 0
+sum_val = arr[-1]
 heapq.heappush(pq, arr[-1])
+
 for k in range(n-2, 0, -1):
     heapq.heappush(pq, arr[k])
+    sum_val += arr[k]
+    min_val = pq[0]
+    avg = (sum_val - min_val)/(n- k -1)
+    if avg > ans :
+        ans = avg
     
-    ans = max(ans, sum(list(pq[1:]))/(len(pq)-1) )
-    
-
-
 print(f"{ans:.2f}")
