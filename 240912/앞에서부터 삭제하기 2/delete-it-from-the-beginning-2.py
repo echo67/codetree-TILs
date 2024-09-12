f0e0e50ac9516176882ebdc5ab191a89c977b21f
echo67 <43358633+echo67@users.noreply.github.com>
2024-09-12 22:02:33 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5030ms, Memory: 40MB, Status: Time Limit Exceed - CodeTree

--- 240912/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240912/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -9,9 +9,7 @@ heapq.heappush(pq, arr[-1])
 for k in range(n-2, 0, -1):
     heapq.heappush(pq, arr[k])
     
-    min_pop = heapq.heappop(pq)
-    ans = max(ans, sum(list(pq))/len(pq) )
-    heapq.heappush(pq, min_pop)
+    ans = max(ans, sum(list(pq[1:]))/(len(pq)-1) )
     
 
 
