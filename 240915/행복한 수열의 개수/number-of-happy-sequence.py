43020c9a732e9de6c827d2afdfac401d36c95f18
echo67 <43358633+echo67@users.noreply.github.com>
2024-09-15 09:24:46 +0900

    Title: 행복한 수열의 개수, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240915/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240915/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,6 +1,5 @@
 n, m = list(map(int, input().split()))
 arr = [list(map(int, input().split())) for _ in range(n)]
-s = set()
 
 same = 0
 ans = 0
@@ -9,15 +8,19 @@ for i in range(n):
     for j in range(n-1):
         if arr[i][j] == arr[i][j+1]:
             same +=1
-    if same >= m-1:
-        ans += 1
+            if same + 1 >= m:
+                ans += 1
+        else:
+            same = 0
 
 for j in range(n):
     same = 0
     for i in range(n-1):
         if arr[i][j] == arr[i+1][j]:
             same +=1
-    if same >= m-1:
-        ans += 1
+            if same + 1 >= m:
+                ans += 1
+        else:
+            same = 0
 
 print(ans)
