ce26100cc88c8f927178f8eb696fc0adcf9bf41c
echo67 <43358633+echo67@users.noreply.github.com>
2024-09-15 09:55:45 +0900

    Title: 행복한 수열의 개수, Time: 168ms, Memory: 25MB, Status: Passed - CodeTree

--- 240915/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240915/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,25 +2,31 @@ n, m = list(map(int, input().split()))
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 same = 0
+max_same = 1
 ans = 0
 for i in range(n):
-    same = 0
+    max_same = 1
+    same = 1
     for j in range(n-1):
         if arr[i][j] == arr[i][j+1]:
             same +=1
-            if same + 1 >= m:
-                ans += 1
         else:
-            same = 0
+            same = 1
+        max_same = max(same, max_same)
+        
+    if  max_same >= m:
+        ans += 1
 
 for j in range(n):
-    same = 0
+    max_same = 1
+    same = 1
     for i in range(n-1):
         if arr[i][j] == arr[i+1][j]:
             same +=1
-            if same + 1 >= m:
-                ans += 1
         else:
-            same = 0
+            same = 1
+        max_same = max(same, max_same)
+    if max_same >= m:
+        ans += 1
 
 print(ans)
