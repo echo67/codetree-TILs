068f1c689460f49acecbab7190c5b7c3c890af6d
echo67 <43358633+echo67@users.noreply.github.com>
2024-09-19 10:17:40 +0900

    Title: 금 채굴하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240919/금 채굴하기/gold-mining.py
@@ -0,0 +1,51 @@
+n, m = tuple(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(n)]
+OUT_OF_MARGIN = None
+
+def margin(x, dx, y, dy, n):
+    if x + dx > n-1:
+        nx = OUT_OF_MARGIN
+    elif x + dx < 0:
+        nx = OUT_OF_MARGIN
+    else: 
+        nx = x + dx
+
+    if y + dy > n-1:
+        ny = OUT_OF_MARGIN
+    elif y + dy < 0:
+        ny = OUT_OF_MARGIN
+    else: 
+        ny = y + dy
+    return (ny, nx)
+
+
+def square(y, x, k):
+    gold = 0
+    for dy in range(-k, k+1):
+        if dy <= 0:
+            for dx in range(-k-dy, dy+k+1):
+                ny, nx = margin(x, dx, y, dy, n)
+                if ny == OUT_OF_MARGIN or nx == OUT_OF_MARGIN:
+                    gold += 0
+                else:
+                    gold += arr[ny][nx]
+        if dy > 0:
+            for dx in range(-k+dy, -dy+k+1):
+                ny, nx = margin(x, dx, y, dy, n)
+                if ny == OUT_OF_MARGIN or nx == OUT_OF_MARGIN:
+                    gold += 0
+                else:
+                    gold += arr[ny][nx]
+    profit = gold * m - k*k - (k+1) * (k+1)
+    return (gold, profit)
+
+max_profit = 0
+max_gold = 0
+for y in range(n):
+    for x in range(n):
+        for k in range(n):
+            gold, profit = square(y, x, k)
+            if profit > 0 and gold > max_gold:
+                max_gold, _ = gold, profit
+
+print(max_gold)
