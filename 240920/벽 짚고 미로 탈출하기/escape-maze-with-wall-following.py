986750a3ca15eaba79df23c4747b767d71ee3ce6
echo67 <43358633+echo67@users.noreply.github.com>
2024-09-20 22:57:35 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 181ms, Memory: 25MB, Status: Passed - CodeTree

--- 240920/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240920/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,17 +1,27 @@
 n = int(input())
 
 y, x = tuple(map(int, input().split()))
-y, x = y-1, x-1
-init_y, init_x = y, x
+curr_y, curr_x = y-1, x-1
 
 arr = [list(input()) for _ in range(n)]
-
-dys = [0, -1, 0, 1]
-dxs = [1, 0, -1, 0]
+elapsed_time = 0
+curr_dir = 0
 
 def in_range(y, x):
     return 0 <= y < n and 0 <= x < n
-move_dir = 0
+
+def wall_exists(y, x):
+    return in_range(y, x) and arr[y][x] == '#'
+
+
+def right_wall_check(y, x):
+    wall_dir = (move_dir - 1 + 4) % 4
+    ny, nx = y+dys[wall_dir], x+dxs[wall_dir]   
+    return in_range(ny, nx) and arr[ny][nx] =='#'
+
+def front_wall_check(y, x):
+    ny, nx = y+dys[move_dir], x+dxs[move_dir]
+    return in_range(ny, nx) and arr[ny][nx] == '#'
 
 # 오른쪽으로 벽 없을 때 시계방향으로 
 def no_right_wall():
@@ -22,34 +32,44 @@ def no_right_wall():
 def front_wall():
     global y, x, move_dir
     move_dir = (move_dir + 1) % 4
-    y, x = y + dys[move_dir], x + dxs[move_dir]
 
-def right_wall_check():
-    wall_dir = (move_dir - 1 + 4) % 4
-    ny, nx = y+dys[wall_dir], x+dxs[wall_dir]   
-    return in_range(ny, nx) and arr[ny][nx] =='#'
-
-def front_wall_check():
-    ny, nx = y+dys[move_dir], x+dxs[move_dir]
-    return in_range(ny, nx) and arr[ny][nx] == '#'
-
-
-
-step = 0
-step_set = set()
-while in_range(y, x):
-    step_set.add((y, x))
-    if front_wall_check():
-        front_wall()
-        step += 1
-    elif right_wall_check():
+    if front_wall_check(y, x):
         y, x = y + dys[move_dir], x + dxs[move_dir]
-        step += 1
     else:
-        no_right_wall()
-        step += 1
-    if (y, x) in step_set:
-        step = -1
+        front_wall()
+
+
+
+step_set = set()
+while in_range(curr_y, curr_x):
+    if (curr_y, curr_x, curr_dir) in step_set:
+        elapsed_time = -1
         break
     
-print(step)
+    step_set.add((curr_y, curr_x, curr_dir))
+    
+    dys = [0, 1, 0, -1]
+    dxs = [1, 0, -1, 0]
+
+    ny, nx = curr_y + dys[curr_dir], curr_x + dxs[curr_dir]
+
+    if wall_exists(ny, nx):
+        curr_dir = (curr_dir - 1 + 4) % 4
+
+    elif not in_range(ny, nx):
+        elapsed_time += 1
+        curr_y, curr_x = ny, nx
+
+    else:
+        ry = ny + dys[(curr_dir + 1)%4]
+        rx = nx + dxs[(curr_dir + 1)%4]
+
+        if wall_exists(ry, rx):
+            curr_y, curr_x = ny, nx
+            elapsed_time += 1
+        else:
+            curr_y, curr_x = ry, rx
+            curr_dir = (curr_dir + 1) % 4
+            elapsed_time += 2
+
+print(elapsed_time)
