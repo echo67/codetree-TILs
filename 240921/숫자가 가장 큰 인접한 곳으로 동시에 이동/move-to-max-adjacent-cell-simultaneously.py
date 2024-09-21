51d174bb31dcc0ab2505a4813aae16beab455446
echo67 <43358633+echo67@users.noreply.github.com>
2024-09-21 12:56:56 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 219ms, Memory: 26MB, Status: Passed - CodeTree

--- 240921/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240921/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -2,7 +2,7 @@
 n, m, t = tuple(map(int, input().split()))
 # 가장 큰 값
 # 우선순위: 상하좌우
-dys, dxs = [1, -1, 0, 0], [0, 0, -1, 1]
+dys, dxs = [-1, 1, 0, 0], [0, 0, -1, 1]
 arr = [list(map(int, input().split())) for _ in range(n)]
 curr_pos = [tuple(map(int, input().split())) for _ in range(m)]
 
@@ -25,25 +25,24 @@ def move_next(y, x):
         if in_range(ny, nx) and arr[ny][nx] > max_num:
             max_num = arr[ny][nx]
             max_pos = (ny, nx)
+    # print(y, x, max_num, max_pos)
     return max_pos
 
 
 for _ in range(t):
-    next_pos = []
     next_count = [[0 for _ in range(n)] for _ in range(n)]
 
-    for elem in curr_pos:
-        y, x = elem
-        ny, nx = move_next(y-1, x-1)
-        next_count[ny][nx] += 1
-        next_pos.append((ny+1, nx+1))
-
-    count = next_count
+    for y in range(n):
+        for x in range(n):
+            if count[y][x] == 1:
+                ny, nx = move_next(y, x)
+                next_count[ny][nx] += 1
+    # print(count)
+    count = [[next_count[i][j] for j in range(n)] for i in range(n)]
 
     for i in range(n):
         for j in range(n):
             if next_count[i][j] >=2:
-                count[i][j] = 0
-                
-    curr_pos = next_pos
+                count[i][j] = 0     
+   
 print(sum([count[i][j] for i in range(n) for j in range(n)] ))
