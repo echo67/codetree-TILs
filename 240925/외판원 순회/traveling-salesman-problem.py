232e82c13c1f90cc90fd435e3b7e627ca94c50d2
echo67 <43358633+echo67@users.noreply.github.com>
2024-09-25 22:30:21 +0900

    Title: 외판원 순회, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,34 @@
+n = int(input())
+a = [ list(map(int, input().split())) for _ in range(n)]
+
+visited = [False] * n
+selected = []
+
+ans = 10000*100
+def find_min(curr):
+    global ans
+    if curr == n - 1:
+        sum_val = a[0][selected[0]-1]
+        for i in range(n-2):
+            sum_val += a[selected[i]-1][selected[i+1]-1]
+        sum_val += a[selected[-1]-1][0]
+        ans = min(ans, sum_val)
+        return
+
+    for i in range(2, n+1):
+        if visited[i-2]:
+            continue
+
+        selected.append(i)
+        visited[i-2] = True
+        
+        find_min(curr + 1)
+        
+        selected.pop()
+        visited[i-2] = False
+
+
+find_min(0)    
+print(ans)
+
+# 2 3 4 중에서 선택
